35836870b3e65972a1fdd503be7b36cc4fc10bc3
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2018-03-31 23:13:08 -0400

    Creating handout file, restructuring demo

--- FlapPyKinect.py
+++ FlapPyKinect.py
@@ -22,61 +22,62 @@ class GameRuntime(object):
         self.flap = 0
 
         # Used to manage how fast the screen updates
-        self._clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
         # Set the width and height of the window [width/2, height/2]
-        self._screen = pygame.display.set_mode((960,540), pygame.HWSURFACE|pygame.DOUBLEBUF, 32)
+        self.screen = pygame.display.set_mode((960,540), pygame.HWSURFACE|pygame.DOUBLEBUF, 32)
 
         # Loop until the user clicks the close button.
-        self._done = False
+        self.done = False
 
         # Kinect runtime object, we want color and body frames 
-        self._kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color | PyKinectV2.FrameSourceTypes_Body)
+        self.kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color | PyKinectV2.FrameSourceTypes_Body)
 
         # back buffer surface for getting Kinect color frames, 32bit color, width and height equal to the Kinect color frame size
-        self._frame_surface = pygame.Surface((self._kinect.color_frame_desc.Width, self._kinect.color_frame_desc.Height), 0, 32)
+        self.frame_surface = pygame.Surface((self.kinect.color_frame_desc.Width, self.kinect.color_frame_desc.Height), 0, 32)
 
         # here we will store skeleton data 
-        self._bodies = None
+        self.bodies = None
 
     def draw_bird(self):
-        pygame.draw.circle(self._frame_surface, (200,200,0), (int(self.screen_width/2), int(self.screen_height - self.bird_height)), 40)
+        pygame.draw.circle(self.frame_surface, (200,200,0), (int(self.screen_width/2), int(self.screen_height - self.bird_height)), 40)
 
     def draw_pipes(self):
         # top pipe
-        pygame.draw.rect(self._frame_surface, (0, 150, 0), (self.pipe_x, 0, 200, 400))
+        pygame.draw.rect(self.frame_surface, (0, 150, 0), (self.pipe_x, 0, 200, 400))
 
         # bottom pipe
-        pygame.draw.rect(self._frame_surface, (0, 150, 0), (self.pipe_x, self.screen_height - 400, 200, 400))
+        pygame.draw.rect(self.frame_surface, (0, 150, 0), (self.pipe_x, self.screen_height - 400, 200, 400))
 
     def draw_color_frame(self, frame, target_surface):
         target_surface.lock()
-        address = self._kinect.surface_as_array(target_surface.get_buffer())
+        address = self.kinect.surface_as_array(target_surface.get_buffer())
+        # moving 
         ctypes.memmove(address, frame.ctypes.data, frame.size)
         del address
         target_surface.unlock()
 
     def run(self):
         # -------- Main Program Loop -----------
-        while not self._done:
+        while not self.done:
             # --- Main event loop
             for event in pygame.event.get(): # User did something
                 if event.type == pygame.QUIT: # If user clicked close
-                    self._done = True # Flag that we are done so we exit this loop
+                    self.done = True # Flag that we are done so we exit this loop
 
             # We have a color frame. Fill out back buffer surface with frame's data 
-            if self._kinect.has_new_color_frame():
-                frame = self._kinect.get_last_color_frame()
-                self.draw_color_frame(frame, self._frame_surface)
+            if self.kinect.has_new_color_frame():
+                frame = self.kinect.get_last_color_frame()
+                self.draw_color_frame(frame, self.frame_surface)
                 frame = None
 
             # We have a body frame, so can get skeletons
-            if self._kinect.has_new_body_frame(): 
-                self._bodies = self._kinect.get_last_body_frame()
+            if self.kinect.has_new_body_frame(): 
+                self.bodies = self.kinect.get_last_body_frame()
 
-                if self._bodies is not None: 
-                    for i in range(0, self._kinect.max_body_count):
-                        body = self._bodies.bodies[i]
+                if self.bodies is not None: 
+                    for i in range(0, self.kinect.max_body_count):
+                        body = self.bodies.bodies[i]
                         if not body.is_tracked: 
                             continue 
                     
@@ -120,22 +121,22 @@ class GameRuntime(object):
             # Optional debugging text
             #font = pygame.font.Font(None, 36)
             #text = font.render(str(self.flap), 1, (0, 0, 0))
-            #self._frame_surface.blit(text, (100,100))
+            #self.frame_surface.blit(text, (100,100))
 
             # --- copy back buffer surface pixels to the screen, resize it if needed and keep aspect ratio
             # --- (screen size may be different from Kinect's color frame size) 
-            h_to_w = float(self._frame_surface.get_height()) / self._frame_surface.get_width()
-            target_height = int(h_to_w * self._screen.get_width())
-            surface_to_draw = pygame.transform.scale(self._frame_surface, (self._screen.get_width(), target_height));
-            self._screen.blit(surface_to_draw, (0,0))
+            h_to_w = float(self.frame_surface.get_height()) / self.frame_surface.get_width()
+            target_height = int(h_to_w * self.screen.get_width())
+            surface_to_draw = pygame.transform.scale(self.frame_surface, (self.screen.get_width(), target_height));
+            self.screen.blit(surface_to_draw, (0,0))
             surface_to_draw = None
             pygame.display.update()
 
             # --- Limit to 60 frames per second
-            self._clock.tick(60)
+            self.clock.tick(60)
 
         # Close our Kinect sensor, close the window and quit.
-        self._kinect.close()
+        self.kinect.close()
         pygame.quit()
 
 game = GameRuntime();
---
+++ handout.py
@@ -0,0 +1,76 @@
+from pykinect2 import PyKinectV2, PyKinectRuntime
+from pykinect2.PyKinectV2 import *
+
+import ctypes
+import _ctypes
+import pygame
+import sys
+import math
+
+
+
+
+class GameRuntime(object):
+    def __init__(self):
+        pygame.init()
+
+
+        # Used to manage how fast the screen updates
+        self.clock = pygame.time.Clock()
+
+        # Loop until the user clicks the close button.
+        self.done = False
+
+        # Kinect runtime object, we want color and body frames 
+        self.kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color | PyKinectV2.FrameSourceTypes_Body)
+
+        # back buffer surface for getting Kinect color frames, 32bit color, width and height equal to the Kinect color frame size
+        self.frame_surface = pygame.Surface((self.kinect.color_frame_desc.Width, self.kinect.color_frame_desc.Height), 0, 32)
+
+        # here we will store skeleton data 
+        self.bodies = None
+
+    def draw_color_frame(self, frame, target_surface):
+        target_surface.lock()
+        address = self.kinect.surface_as_array(target_surface.get_buffer())
+        ctypes.memmove(address, frame.ctypes.data, frame.size)
+        del address
+        target_surface.unlock()
+
+    def run(self):
+        # -------- Main Program Loop -----------
+        while not self.done:
+            # --- Main event loop
+            for event in pygame.event.get(): # User did something
+                if event.type == pygame.QUIT: # If user clicked close
+                    self.done = True # Flag that we are done so we exit this loop
+
+            # We have a color frame. Fill out back buffer surface with frame's data 
+            if self.kinect.has_new_color_frame():
+                frame = self.kinect.get_last_color_frame()
+                self.draw_color_frame(frame, self.frame_surface)
+                frame = None # memory save
+
+            # We have a body frame, so can get skeletons
+            if self.kinect.has_new_body_frame(): 
+                self.bodies = self.kinect.get_last_body_frame()
+                # TODO: Handle body input!
+
+            # --- copy back buffer surface pixels to the screen, resize it if needed and keep aspect ratio
+            # --- (screen size may be different from Kinect's color frame size) 
+            h_to_w = float(self.frame_surface.get_height()) / self.frame_surface.get_width()
+            target_height = int(h_to_w * self.screen.get_width())
+            surface_to_draw = pygame.transform.scale(self.frame_surface, (self.screen.get_width(), target_height));
+            self.screen.blit(surface_to_draw, (0,0))
+            surface_to_draw = None # memory save
+            pygame.display.update()
+
+            # --- Limit to 60 frames per second
+            self.clock.tick(60)
+
+        # Close our Kinect sensor, close the window and quit.
+        self.kinect.close()
+        pygame.quit()
+
+game = GameRuntime();
+game.run();
